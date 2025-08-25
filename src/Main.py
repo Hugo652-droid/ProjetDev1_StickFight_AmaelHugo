d07c6019c88cbcf819589f00e653b852258a47cb
pb72yvp <143070157+Hugo652-droid@users.noreply.github.com>
2025-08-25 11:58:39 +0200

    Add function to add a charchter and move it

--- src/Main.py
+++ src/Main.py
@@ -1,7 +1,7 @@
 import pygame
 
 from src.Root import Root
-
+from src.Player import Player
 
 class Main():
     def __init__(self):
@@ -19,13 +19,29 @@ class Main():
                         runningHome = False
 
         windowGame = Root(pygame)
-        windowGame.changeColor((255, 255, 0))
+        windowGame.changeColor((255, 255, 255))
+        playerActif = Player("Amael", 100, 1, 100, 100)
         runningGame = True
         while runningGame:
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         windowGame.closeRoot(pygame)
+
+            keys = pygame.key.get_pressed()
+
+            if keys[pygame.K_a]:
+                playerActif.x -= 1
+            if keys[pygame.K_d]:
+                playerActif.x += 1
+            if keys[pygame.K_w]:
+                playerActif.y -= 1
+            if keys[pygame.K_s]:
+                playerActif.y += 1
+
+
+            playerActif.draw(windowGame.screen, "images/test_stick.png")
+            pygame.display.flip()
 
 
 
--- src/Player.py
+++ src/Player.py
@@ -1,7 +1,15 @@
+import pygame
+
+
 class Player:
-    def __init__(self, name, hp, weapon_id):
+    def __init__(self, name, hp, weapon_id, x, y):
         self.name = name
         self.hp = hp
         self.weapon_id = weapon_id
+        self.x = x
+        self.y = y
 
-
+    def draw(self, screen, image):
+        image_a_afficher = pygame.image.load(image)
+        image_convertie = image_a_afficher.convert()
+        screen.blit(image_convertie, (self.x, self.y))
