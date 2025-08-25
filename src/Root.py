641a5945ffef68aac0dd2256d17a567873038989
amaeljampen <amael.jampen@eduvaud.ch>
2025-08-25 11:18:12 +0200

    try change bg

--- src/Root.py
+++ src/Root.py
@@ -11,13 +11,25 @@ class Root:
         largeur_ecran = 1920
         hauteur_ecran = 1080
         taille_fenetre = (largeur_ecran, hauteur_ecran)
-        screen = pygame.display.set_mode(taille_fenetre)
+        self.screen = pygame.display.set_mode(taille_fenetre)
 
         # Nom de la fenêtre
         pygame.display.set_caption("SticK.Onion")
 
     def closeRoot(self, pygame):
         pygame.display.quit()
+
+    def changeBg(self):
+        # Charge l'image depuis le fichier 'mon_image.png'
+        image_a_afficher = pygame.image.load('images/img.png')
+
+        # Convertit l'image dans un format optimal pour l'affichage
+        image_convertie = image_a_afficher.convert()
+
+        self.screen.blit(image_convertie, (100, 50))
+
+        pygame.display.flip()
+
 
 
 
