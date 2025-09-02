3f70ae30827105df410a2e45e38c3199d91a04fa
pb72yvp <143070157+Hugo652-droid@users.noreply.github.com>
2025-09-02 08:39:03 +0200

    fix bullets

--- src/Bullets.py
+++ src/Bullets.py
@@ -5,6 +5,7 @@ class Bullet:
     def __init__(self, screen):
         self.screen = screen
         self.image = pygame.image.load('images/img_bullets.jpg').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (50, 20))
         self.rect = self.image.get_rect()
 
 
