04931b448ee4366a0ad0cb07af4154c3491dc65d
amaeljampen <amael.jampen@eduvaud.ch>
2025-09-02 14:13:20 +0200

    sol

--- src/Map.py
+++ src/Map.py
@@ -1,15 +1,17 @@
 class Map:
-    def __init__(self, window, pygame, x, y):
+    def __init__(self, window, pygame, x, y, w, h):
         self.pygame = pygame
         self.window = window
         self.img_floor = self.pygame.image.load('./images/floor_test.png')
         self.x = x
         self.y = y
+        self.w = w
+        self.h = h
         self.rect = self.img_floor.get_rect()
         self.info_screen = pygame.display.Info()
 
     def draw(self, screen):
-        self.img_floor = self.pygame.transform.scale(self.img_floor, ((self.info_screen.current_w-200), 200))
+        self.img_floor = self.pygame.transform.scale(self.img_floor, (self.w, self.h))
         self.rect = self.img_floor.get_rect()
         self.rect.center = (self.x, self.y)
         screen.blit(self.img_floor, self.rect)
