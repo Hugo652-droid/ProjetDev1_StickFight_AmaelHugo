e5d2aa9860610b261834165d62ee30da892ff224
pb72yvp <143070157+Hugo652-droid@users.noreply.github.com>
2025-09-05 10:41:56 +0200

    modif main, game, home

--- src/Home.py
+++ src/Home.py
@@ -1,31 +1,46 @@
+"""
+--
+Auteur : Amael Rochat et Hugo Rod
+Date de départ : 18.08.2025
+Date de fin : --.--.----
+Projet : Projet Dev 1 (sticKOnion)
+--
+Nom fichier : Main.py
+Description fichier : Affichage de la page d'accueil
+--
+"""
+
 import pygame
 from src.Root import Root
 from src.Game import Game
 
 class Home :
     def __init__(self):
-        windowHome = Root(pygame)
-        windowHome.changeBg('images/img_bg_main.png')
-        windowHome.buttonPlay()
-        windowHome.buttonQuit()
-        windowHome.title('images/Image titre.png')
+        self.windowHome = Root(pygame)
+        self.windowHome.changeBg('images/img_bg_main.png')
+        self.windowHome.buttonPlay()
+        self.windowHome.buttonQuit()
+        self.windowHome.title('images/Image titre.png')
         self.font = pygame.font.SysFont('Arial', 25)
-        windowHome.version(self.font)
+        self.windowHome.version(self.font)
+        
+    def launch(self):
         pygame.display.flip()
         runningHome = True
         while runningHome:
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
-                        windowHome.closeRoot(pygame)
+                        self.windowHome.closeRoot(pygame)
                         runningHome = False
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if windowHome.button_rect.collidepoint(event.pos):
-                        windowHome.closeRoot(pygame)
+                    if self.windowHome.button_rect.collidepoint(event.pos):
+                        self.windowHome.closeRoot(pygame)
                         runningHome = False
                         game = Game()
+                        game.launchGame()
 
-                    if windowHome.button_rect_quit.collidepoint(event.pos):
-                        windowHome.closeRoot(pygame)
+                    if self.windowHome.button_rect_quit.collidepoint(event.pos):
+                        self.windowHome.closeRoot(pygame)
                         runningHome = False
